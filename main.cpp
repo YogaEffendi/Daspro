9166734348cc2b99cf9e17deb07ae227f66da678
YogaEffendi <112881719+YogaEffendi@users.noreply.github.com>
2022-09-06 21:32:00 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama           :Yoga Effendi Saputra" << endl;
+    cout << "NIM            :A11.2022.14129" << endl;
+    cout << "Alamat         :Baran, Ambarawa, Kab Semarang" << endl;
+    cout << "Asal Sekolah   :SMA Negeri 1 Ambarawa" << endl;
+    return 0;
+}
