4fc0d52c9a83dff134ab49ec30a0fc489c6cc30c
YogaEffendi <112881719+YogaEffendi@users.noreply.github.com>
2022-09-13 21:11:31 +0700

    Add files via upload

---
+++ Pertemuan3/Pertemuan3/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    //printf ("Test")
+
+    int u = 19;
+    //Bilangan bulat
+    float t = 170.5;
+    //Bilangan pecahan/desimal
+    char b = 'L';
+    //1 huruf
+    string ub = "XL";
+    //Lebih dari 1 huruf/kata
+
+    cout << "Umur saya: " << u << endl;
+    cout << "Tinggi saya: " << t;
+    cout << endl;
+    cout << "Ukuran baju: " << b;
+    cout << "\nUkuran baju: " << ub;
+
+    return 0;
+}
